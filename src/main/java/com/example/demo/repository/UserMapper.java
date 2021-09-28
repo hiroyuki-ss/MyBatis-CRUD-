c1f4b9cedd7982c88de11ac99385d3a8b6552c2e
hiroyukisss1 <h.i.7.107932@gmail.com>
2021-09-28 16:20:09 +0900

    select-複数件

--- src/main/java/com/example/demo/repository/UserMapper.java
+++ src/main/java/com/example/demo/repository/UserMapper.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.demo.domain.user.model.MUser;
@@ -11,4 +13,8 @@ public interface UserMapper {
 	//*ユーザー登録
 	public int insertOne(MUser user);
 	
+	//*ユーザー取得
+	//*select文の実行結果が複数件になる場合、メソッドの戻り値をListにする
+	public List<MUser> findMany();
+		
 }
